abbc9409c8fd91756ac436eef3c1223624a13430
Jon Bell <github@lot23.com>
2023-07-23 21:27:22 +1200

    Route

--- app/api/data/route.js
+++
@@ -1,9 +0,0 @@
-import fs from "fs";
-import path from "path";
-
-export default function handler(req, res) {
-  const filePath = path.join(process.cwd(), "output.txt");
-  const fileContents = fs.readFileSync(filePath, "utf8");
-
-  res.status(200).send(fileContents);
-}
