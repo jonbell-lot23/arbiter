a6e0594cc8be09c847796ce299ce4dae7b48b5d6
Jon Bell <github@lot23.com>
2023-07-23 21:23:10 +1200

    Update scrape-memeorandum.js

--- scrape-memeorandum.js
+++ scrape-memeorandum.js
@@ -1,22 +1,25 @@
-const { chromium } = require('playwright');
-const fs = require('fs').promises; // Import the promises version of fs
+const { chromium } = require("playwright");
+const fs = require("fs").promises; // Import the promises version of fs
 
 (async () => {
   const browser = await chromium.launch();
   const context = await browser.newContext();
   const page = await context.newPage();
 
-  await page.goto('https://memeorandum.com/');
-  await page.waitForSelector('div.ii');
+  await page.goto("https://memeorandum.com/");
+  await page.waitForSelector("div.ii");
 
-  const divContents = await page.$$eval('div.ii', divs => divs.map(div => 
-div.textContent));
+  const divContents = await page.$$eval("div.ii", (divs) =>
+    divs.map((div) => div.textContent)
+  );
 
   console.log(divContents);
 
-  // Write divContents to output.txt
-  await fs.writeFile('public/output.txt', divContents.join('\n'));
+  // Write divContents to output.json
+  await fs.writeFile(
+    "public/output.json",
+    JSON.stringify(divContents, null, 2)
+  );
 
   await browser.close();
 })();
-
