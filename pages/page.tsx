796e19ff204af4e5b3c772f1b967b098469e8013
Jon Bell <github@lot23.com>
2023-08-05 14:24:10 +1200

    More tweaks

--- pages/page.tsx
+++ pages/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { supabase } from "../api/supabase";
+import { supabase } from "./api/supabase";
 
 type Post = {
   id: number;
