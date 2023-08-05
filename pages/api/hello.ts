790bc287838ef0e6f1ec97ad1541691329ab9e0e
Jon Bell <github@lot23.com>
2023-08-05 12:07:07 +1200

    TS

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,3 +1,5 @@
-export default function handler(req, res) {
-    res.status(200).json({ text: 'Yeahhhhh I called an API' })
-  }
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    res.status(200).json({ text: 'Yeahhhhh I called an API' });
+}
