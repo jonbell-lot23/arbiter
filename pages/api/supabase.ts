9a3206b50edd5d4091e3ca236a9aa61d5da1e2e8
Jon Bell <github@lot23.com>
2023-08-05 14:32:22 +1200

    Fixes

--- pages/api/supabase.ts
+++ pages/api/supabase.ts
@@ -1,6 +1,11 @@
-import { createClient } from '@supabase/supabase-js'
+// pages/api/supabase.ts
+import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-)
+const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+if (!url || !key) {
+  throw new Error("Missing env vars for Supabase");
+}
+
+export const supabase = createClient(url, key);
