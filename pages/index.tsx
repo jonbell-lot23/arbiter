6fabe05d0fb03ae2e73b1c702efb5ecdfda468c0
Jon Bell <github@lot23.com>
2023-08-06 14:11:52 +1200

    Gray text

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Head from "next/head";
 import { PrismaClient, arbiter_v1 } from "@prisma/client";
 import ReactMarkdown from "react-markdown";
 import "../app/globals.css";
+import styles from "./styles.module.css";
 
 interface Props {
   posts: arbiter_v1WithDate[];
@@ -11,6 +12,7 @@ interface Props {
 
 interface arbiter_v1WithDate extends arbiter_v1 {
   created_at: Date;
+  viewed?: boolean;
 }
 
 const prisma = new PrismaClient();
@@ -37,10 +39,31 @@ export async function getStaticProps() {
 }
 
 const Home: NextPage<Props> = ({ posts }) => {
-  const [sortedPosts, setSortedPosts] = useState<arbiter_v1[]>([]);
+  const [sortedPosts, setSortedPosts] = useState<arbiter_v1WithDate[]>([]);
 
   useEffect(() => {
-    setSortedPosts(posts);
+    console.log("Running useEffect");
+    let viewedArticles = JSON.parse(
+      localStorage.getItem("viewedArticles") || "[]"
+    );
+    console.log("Viewed articles from localStorage:", viewedArticles);
+
+    // Add all new articles to the viewed list
+    const newArticleIds = posts
+      .map((post) => post.id)
+      .filter((id) => !viewedArticles.includes(id));
+    console.log("New article IDs:", newArticleIds);
+    viewedArticles = [...viewedArticles, ...newArticleIds];
+    localStorage.setItem("viewedArticles", JSON.stringify(viewedArticles));
+    console.log("Updated viewed articles:", viewedArticles);
+
+    // Update the state
+    const updatedPosts = posts.map((post) => ({
+      ...post,
+      viewed: viewedArticles.includes(post.id),
+    }));
+    console.log("Updated posts:", updatedPosts);
+    setSortedPosts(updatedPosts);
   }, [posts]);
 
   return (
@@ -58,7 +81,11 @@ const Home: NextPage<Props> = ({ posts }) => {
             {sortedPosts.map((post) => (
               <div
                 key={post.id.toString()}
-                className="p-4 transition-all duration-300 rounded-lg"
+                className={
+                  post.viewed
+                    ? `${styles.article} ${styles.viewed}`
+                    : `${styles.article} ${styles.notViewed}`
+                }
               >
                 <ReactMarkdown
                   className="mx-auto text-sm prose prose-lg"
