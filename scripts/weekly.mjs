42298dc7f5bd8610036c5729e01e38de34fc4c7e
Jon Bell <github@lot23.com>
2023-11-11 10:25:01 +1300

    Turbo?

--- scripts/weekly.mjs
+++ scripts/weekly.mjs
@@ -20,12 +20,12 @@ async function getTranslation(headlines) {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      model: "gpt-3.5-turbo",
+      model: "gpt-4-turbo",
       messages: [
         { role: "system", content: instructions },
         { role: "user", content: headlines.join("\n") },
       ],
-      max_tokens: 200,
+      max_tokens: 2000,
     }),
   });
 
