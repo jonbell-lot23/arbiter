11c95a59961443b1ee3b05532fd12cef2773fe50
Jon Bell <github@lot23.com>
2023-11-12 09:44:23 +1300

    Trying again maybe

--- scripts/weekly.mjs
+++ scripts/weekly.mjs
@@ -12,7 +12,7 @@ const openaiKey = process.env.OPENAI_KEY;
 
 async function getTranslation(headlines) {
   const instructions =
-    "Please summarise the last week's news. These are the headlines to summarise.";
+    "Please summarise the last week's news. I am going to pass along a raw list of headlines, and I want you to sort them based on the most newsworthy first. If a bit of news might make it to the end of the year summary of important events, include it. If it's something we won't even remember in a week or a month, drop it. Give me a good summary for someone who only wants the top line news.";
   const response = await fetch(openaiEndpoint, {
     method: "POST",
     headers: {
@@ -57,9 +57,16 @@ async function getHeadlines() {
 }
 
 async function main() {
-  const headlines = await getHeadlines();
-  const summary = await getTranslation(headlines);
-  console.log(summary);
+  try {
+    console.log("Fetching headlines...");
+    const headlines = await getHeadlines();
+    console.log("Headlines fetched, translating...");
+    const summary = await getTranslation(headlines);
+    console.log("Translation complete. Summary:");
+    console.log(summary);
+  } catch (error) {
+    console.error("Error in main function:", error);
+  }
 }
 
-main();
+main().then(() => console.log("Script completed."));
